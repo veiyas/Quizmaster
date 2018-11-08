ac16f92b7c8ab1b2660d7c7765507f818830f819
David <david.rk97@gmail.com>
2018-11-08 09:21:25 +0100

    prövade att lägga till knappar och bool för höger eller vänsterhänt

--- app/src/main/java/com/example/david/quizmasterandroid/MainActivity.java
+++ app/src/main/java/com/example/david/quizmasterandroid/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.david.quizmasterandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean isRightHanded = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setRightHanded(View firstButton) {
+        isRightHanded = true;
+        Toast.makeText(this, "true", Toast.LENGTH_SHORT).show();
+    }
+
+    public void setLeftHanded(View firstButton) {
+        isRightHanded = false;
+        Toast.makeText(this, "false", Toast.LENGTH_SHORT).show();
     }
 }

69b722d6c376e1e5e1b5b9d2a19beea2a735190b
David <david.rk97@gmail.com>
2018-11-14 17:04:15 +0100

    Nu går det bara att starta spelet om man har valt 4 kategorier

--- app/src/main/java/com/example/david/quizmasterandroid/leftQuizOptions.java
+++ app/src/main/java/com/example/david/quizmasterandroid/leftQuizOptions.java
@@ -1,28 +1,67 @@
 package com.example.david.quizmasterandroid;
 
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
-public class leftQuizOptions extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
 
-    public final int maxCatsChosen = 4;
+public class leftQuizOptions extends AppCompatActivity {
+
+    ArrayList<Switch> theSwitches = new ArrayList<>();
+    TextView errorText;
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_left_quiz_options);
 
-        TextView errorText = (TextView) findViewById(R.id.errorText);
+        //Sätt errorText och avaktivera start knappen från början så att det inte går att starta direkt
+        errorText = (TextView) findViewById(R.id.errorText);
         errorText.setText("Du måste välja fyra kategorier!");
-        Button startButton = (Button) findViewById(R.id.startButton);
-        startButton.setClickable(false); startButton.setBackgroundColor(2);
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setClickable(false); startButton.setText("Inte redo!");
+
+        //Sätt en actionlistener på alla switchar
+        theSwitches.add((Switch) findViewById(R.id.switch1));
+        theSwitches.add((Switch) findViewById(R.id.switch2));
+        theSwitches.add((Switch) findViewById(R.id.switch3));
+        theSwitches.add((Switch) findViewById(R.id.switch4));
+        theSwitches.add((Switch) findViewById(R.id.switch5));
+        theSwitches.add((Switch) findViewById(R.id.switch6));
+
+        for(int i=0; i<theSwitches.size(); i++) {
+            theSwitches.get(i).setOnClickListener(readyCheck);
+        }
     }
+
+    View.OnClickListener readyCheck = new View.OnClickListener() {
+        public void onClick(View v) {
+            int switchesPressed = 0;
+
+            for(int i=0; i<theSwitches.size(); i++) {
+                if(theSwitches.get(i).isChecked()) {
+                    switchesPressed++;
+                }
+            }
+            if(switchesPressed == 4) {
+                errorText.setText("Redo!");
+
+                startButton.setClickable(true); startButton.setText("Start!");
+            }
+            else {
+                errorText.setText("Du måste välja fyra kategorier!");
+
+                startButton.setClickable(false); startButton.setText("Inte redo!");
+            }
+
+        }
+    };
 
 
 
--- app/src/main/java/com/example/david/quizmasterandroid/rightQuizOptions.java
+++ app/src/main/java/com/example/david/quizmasterandroid/rightQuizOptions.java
@@ -4,14 +4,64 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class rightQuizOptions extends AppCompatActivity {
+
+    ArrayList<Switch> theSwitches = new ArrayList<>();
+    TextView errorText;
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_right_quiz_options);
+
+        //Sätt errorText och avaktivera start knappen från början så att det inte går att starta direkt
+        errorText = (TextView) findViewById(R.id.errorText);
+        errorText.setText("Du måste välja fyra kategorier!");
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setClickable(false); startButton.setText("Inte redo!");
+
+        //Sätt en actionlistener på alla switchar
+        theSwitches.add((Switch) findViewById(R.id.switch1));
+        theSwitches.add((Switch) findViewById(R.id.switch2));
+        theSwitches.add((Switch) findViewById(R.id.switch3));
+        theSwitches.add((Switch) findViewById(R.id.switch4));
+        theSwitches.add((Switch) findViewById(R.id.switch5));
+        theSwitches.add((Switch) findViewById(R.id.switch6));
+
+        for(int i=0; i<theSwitches.size(); i++) {
+            theSwitches.get(i).setOnClickListener(readyCheck);
+        }
     }
+
+    View.OnClickListener readyCheck = new View.OnClickListener() {
+        public void onClick(View v) {
+            int switchesPressed = 0;
+
+            for(int i=0; i<theSwitches.size(); i++) {
+                if(theSwitches.get(i).isChecked()) {
+                    switchesPressed++;
+                }
+            }
+            if(switchesPressed == 4) {
+                errorText.setText("Redo!");
+
+                startButton.setClickable(true); startButton.setText("Start!");
+            }
+            else {
+                errorText.setText("Du måste välja fyra kategorier!");
+
+                startButton.setClickable(false); startButton.setText("Inte redo!");
+            }
+
+        }
+    };
 
     public void startTheGame(View view) {
         Intent startIntent = new Intent(rightQuizOptions.this, StartGameActivity.class);
